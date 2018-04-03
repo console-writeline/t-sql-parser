8bc4781e22d8dae362e75553fe531ddfa40297d5
A T <athasarathan@csod.com>
2018-04-02 22:22:21 -0700

    implemented parsing InsertStatement and UpdateStatement

--- TSqlParser.Core/SqlScriptAnalyzer.cs
+++ TSqlParser.Core/SqlScriptAnalyzer.cs
@@ -43,8 +43,8 @@ namespace TSqlParser.Core
             using (StringReader sr = new StringReader(inputScript))
             {
                 fragment = _parser.Parse(sr, out errors);
-                var statmentList = _parser.ParseStatementList(sr, out errors);
             }
+
 
             if (errors.Count > 0)
             {
@@ -55,11 +55,143 @@ namespace TSqlParser.Core
                 TSqlScript sqlScript = fragment as TSqlScript;
                 if (sqlScript != null)
                 {
-
+                    foreach (TSqlBatch batch in sqlScript.Batches)
+                    {
+                        foreach (TSqlStatement statement in batch.Statements)
+                        {
+                            if (statement is CreateProcedureStatement createProcedureStatement)
+                                AnalyzeCreateProcedureStatement(createProcedureStatement, ref results);
+                            else if (statement is BeginEndBlockStatement beginEndBlockStatement)
+                                AnalyzeBeginEndBlockStatement(beginEndBlockStatement, ref results);
+                        }
+                    }
                 }
             }
 
             return await Task.FromResult(results);
+        }
+
+        private void AnalyzeCreateProcedureStatement(CreateProcedureStatement createProcedureStatement, ref ParserResults results)
+        {
+            //((BeginEndBlockStatement)((CreateProcedureStatement)statement).StatementList.Statements[0]).StatementList.Statements
+            if (createProcedureStatement.StatementList.Statements.Count == 0)
+                return;
+
+            foreach (TSqlStatement statement in createProcedureStatement.StatementList.Statements)
+            {
+                if (statement is BeginEndBlockStatement beginEndBlockStatement)
+                    AnalyzeBeginEndBlockStatement(beginEndBlockStatement, ref results);
+            }
+
+        }
+
+        private void AnalyzeBeginEndBlockStatement(BeginEndBlockStatement beginEndBlockStatement, ref ParserResults results)
+        {
+            if (beginEndBlockStatement.StatementList.Statements.Count == 0)
+                return;
+
+            foreach (TSqlStatement statement in beginEndBlockStatement.StatementList.Statements)
+            {
+                if (statement is UpdateStatement updateStatement)
+                    AnalyzeUpdateStatement(updateStatement, ref results);
+                else if (statement is InsertStatement insertStatement)
+                    AnalyzeInsertStatement(insertStatement, ref results);
+                else if (statement is SelectStatement selectStatement)
+                    AnalyzeSelectStatement(selectStatement, ref results);
+                else if (statement is MergeStatement mergeStatement)
+                    AnalyzeMergeStatement(mergeStatement, ref results);
+                else if (statement is DeleteStatement deleteStatement)
+                    AnalyzeDeleteStatement(deleteStatement, ref results);
+            }
+        }
+
+        private void AnalyzeUpdateStatement(UpdateStatement updateStatement, ref ParserResults results)
+        {
+            if (updateStatement.UpdateSpecification.Target is NamedTableReference updateNamedTableReference)
+            {
+                foreach (Identifier tableNameIdentifier in updateNamedTableReference.SchemaObject.Identifiers)
+                {
+                    string tableName = tableNameIdentifier.Value;
+
+                    if (!results.TableParsingResults.Any(x => x.TableName == tableName && x.OperationType == SqlOperationType.UPDATE))
+                    {
+                        results.TableParsingResults.Add(new TableParsingResult()
+                        {
+                            TableName = tableName,
+                            OperationType = SqlOperationType.UPDATE
+                        }); // table names to be updated
+                    }
+                }
+            }
+        }
+
+        private void AnalyzeInsertStatement(InsertStatement insertStatement, ref ParserResults results)
+        {
+            if (insertStatement.InsertSpecification.Target is NamedTableReference insertNamedTableReference)
+            {
+                foreach (Identifier tableNameIdentifier in insertNamedTableReference.SchemaObject.Identifiers)
+                {
+                    string tableName = tableNameIdentifier.Value;
+                    if (!results.TableParsingResults.Any(x => x.TableName == tableName && x.OperationType == SqlOperationType.UPDATE))
+                    {
+                        results.TableParsingResults.Add(new TableParsingResult()
+                        {
+                            TableName = tableName,
+                            OperationType = SqlOperationType.INSERT
+                        });
+                    }
+                }
+            }
+        }
+
+        private void AnalyzeSelectStatement(SelectStatement selectStatement, ref ParserResults results)
+        {
+
+        }
+
+        private void AnalyzeMergeStatement(MergeStatement mergeStatement, ref ParserResults results)
+        {
+
+        }
+
+        private void AnalyzeDeleteStatement(DeleteStatement deleteStatement, ref ParserResults results)
+        {
+
+        }
+
+        private void ExtractTablesUsedInFromClause(List<string> selectTables, FromClause fromClause)
+        {
+            foreach (QualifiedJoin tableJoinReference in fromClause.TableReferences)
+            {
+                QualifiedJoin joinReference = tableJoinReference;
+                do
+                {
+                    if (joinReference.SecondTableReference is NamedTableReference namedTableReference)
+                    {
+                        foreach (Identifier tableNameIdentifier in namedTableReference.SchemaObject.Identifiers)
+                        {
+                            if (!selectTables.Exists(x => x == tableNameIdentifier.Value.Trim().ToLower()))
+                            {
+                                selectTables.Add(tableNameIdentifier.Value.Trim().ToLower());
+                            }
+                        }
+                    }
+
+                    if (joinReference.FirstTableReference is NamedTableReference namedTableReference2)
+                    {
+                        foreach (Identifier tableNameIdentifier in namedTableReference2.SchemaObject.Identifiers)
+                        {
+                            if (!selectTables.Exists(x => x == tableNameIdentifier.Value.Trim().ToLower()))
+                            {
+                                selectTables.Add(tableNameIdentifier.Value.Trim().ToLower());
+                            }
+                        }
+                    }
+
+                    joinReference = joinReference.FirstTableReference as QualifiedJoin;
+                }
+                while (joinReference != null);
+            }
         }
 
     }
@@ -69,6 +201,7 @@ namespace TSqlParser.Core
     /// </summary>
     public class ParserResults
     {
+        
         /// <summary>
         /// Gets or sets the name of the stored procedure referenced in SQL text.
         /// </summary>
@@ -92,7 +225,7 @@ namespace TSqlParser.Core
         /// <value>
         /// The table parsing results.
         /// </value>
-        public List<TableParsingResult> TableParsingResults { get; set; }
+        public List<TableParsingResult> TableParsingResults { get; set; } = new List<TableParsingResult>();
 
         /// <summary>
         /// Gets a value indicating whether this instance has parsing exception.
@@ -154,7 +287,7 @@ namespace TSqlParser.Core
         /// <value>
         /// The column parsing results.
         /// </value>
-        public List<ColumnParsingResult> ColumnParsingResults { get; set; }
+        public List<ColumnParsingResult> ColumnParsingResults { get; set; } = new List<ColumnParsingResult>();
     }
 
     /// <summary>
